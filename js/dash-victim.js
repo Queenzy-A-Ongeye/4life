9d7140a871221297fbfebe5c24e15fae16769185
layersony <sammaingi5@gmail.com>
2021-04-26 18:09:18 +0300

    chat section working

---
+++ js/dash-victim.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("#chat").click(function(){
+      document.getElementById("mySideChat").style.width = "450px";
+  })
+  $("#closeChat").click(function(){
+    document.getElementById("mySideChat").style.width = "0px";
+})
+})
