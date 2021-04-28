d5436d9c1cdf7baf58ae1bddc5c8689b3b0cc1d2
layersony <sammaingi5@gmail.com>
2021-04-28 11:24:56 +0300

    permanent localstorage dynamic

--- js/dash-victim.js
+++ js/dash-victim.js
@@ -1,3 +1,36 @@
+// retrive data
+function Booking(date, time, view){
+  this.date = date;
+  this.time = time;
+  this.view = view;
+}
+
+
+let getobjects = function(userkey){
+  return JSON.parse(localStorage.getItem(userkey))
+}
+
+let saveObjects = function(userkey,uservalue){
+  let str = JSON.stringify(uservalue);
+  return localStorage.setItem(userkey, str)
+}
+
+let def = 0
+window.onload = function(){
+  if (localStorage.length === 0){
+    saveObjects('default', def);
+    location.reload();
+  }else{
+    def = localStorage.length
+    $("#confr").on('click', function(){
+      def += 1
+      saveObjects('default', def)
+      console.log('saved')
+    })
+  }
+}
+
+// user logic
 $(document).ready(function(){
   $("#chat").click(function(){
       document.getElementById("mySideChat").style.width = "450px";
@@ -5,12 +38,21 @@ $(document).ready(function(){
   $("#closeChat").click(function(){
     document.getElementById("mySideChat").style.width = "0px";
   })
+
   // book session
   $('#confr').on('click', function(){
-    let bookcal = $("")
+    let bookcal = $("#bkmet").val();
     let bTime = $("#appt").val();
     let hSession = $("input:radio[name=tlk]:checked").val()
 
-    
+    if (bookcal && bTime && hSession){
+      let newbooking = new Booking(bookcal, bTime, hSession);
+      saveObjects('user'+def, newbooking);
+    }
+
+    for (let i = 1; i<localStorage.length; i++){
+      let bookinfo = getobjects('user'+i)
+      $("ul#lstBkDply").append("<li class ='moreDetails'>" + bookinfo.date +"--"+bookinfo.time+'--'+ bookinfo.view + "</li>");
+    }
   });
-})
+});
