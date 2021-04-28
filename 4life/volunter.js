0b85b19cd62856cbaeda8fd1028074fc29530e9b
sngina <sngina707@gmail.com>
2021-04-28 17:52:03 +0300

    add html tag

--- 4life/volunter.js
+++ 4life/volunter.js
@@ -5,5 +5,21 @@ $(document).ready(function(){
     $("#closeChat").click(function(){
       document.getElementById("mySideChat").style.width = "0px";
     })
-$()
-});
+    function volunter(fullName ,email ,professional, specialisation) {
+
+      this.fullName = fullName;
+      this.email = email;
+      this.professional = professional;
+      this.specialisation = specialisation;
+      $(document).ready(function(){
+        $("#contact").submit(function(event){
+            event.preventDefault();
+            var userFullName = $("#").val();
+            var userEmail = $("input#lastName").val();
+            var userProfessional = $("input#email").val();
+            var userSpecialisation = parseInt($("input#phone").val());
+    
+    })
+  
+    
+})
