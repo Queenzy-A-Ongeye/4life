4c0706218000297bebc7b54da399f516e6632a2c
sngina <sngina707@gmail.com>
2021-04-29 09:47:37 +0300

    add a html file

--- 4life/volunter.js
+++ 4life/volunter.js
@@ -1,25 +1,51 @@
-$(document).ready(function(){
-    $("#chat").click(function(){
-        document.getElementById("mySideChat").style.width = "450px";
+function Volunter(first, last, email, phone, profession, special) {
+  this.first = first;
+  this.last = last;
+  this.email = email;
+  this.phone = phone;
+  this.profession = profession;
+  this.special = special;
+}
+Volunter.prototype.fullname = function () {
+  return this.first + " " + this.last
+}
+let getobjects = function (userkey) {
+  return JSON.parse(localStorage.getItem(userkey))
+}
+let saveObjects = function (userkey, uservalue) {
+  let str = JSON.stringify(uservalue);
+  return localStorage.setItem(userkey, str)
+}
+let vdef = 0
+window.onload = function () {
+  if (localStorage.length === 0) {
+    saveObjects('Vdefault', vdef);
+    location.reload();
+  } else {
+    vdef = localStorage.length
+    $("#confr").on('click', function () {
+      vdef += 1
+      saveObjects('default', vdef)
     })
-    $("#closeChat").click(function(){
-      document.getElementById("mySideChat").style.width = "0px";
-    })
-    function volunter(fullName ,email ,professional, specialisation) {
-
-      this.fullName = fullName;
-      this.email = email;
-      this.professional = professional;
-      this.specialisation = specialisation;
-      $(document).ready(function(){
-        $("#contact").submit(function(event){
-            event.preventDefault();
-            var userFullName = $("#").val();
-            var userEmail = $("input#lastName").val();
-            var userProfessional = $("input#email").val();
-            var userSpecialisation = parseInt($("input#phone").val());
-    
-    })
-  
-    
+  }
+}
+$(document).ready(function () {
+  $("#chat").click(function () {
+    document.getElementById("mySideChat").style.width = "450px";
+  })
+  $("#closeChat").click(function () {
+    document.getElementById("mySideChat").style.width = "0px";
+  })
+  $("#contact").on('click',function(){
+    var volFirstName = $("#firstName").val();
+    var volSecondName = $("input#lastName").val();
+    var volEmail = $("input#email").val();
+    var volNumber = parseInt($("input#phone").val());
+    var volProf = $("#prof").val();
+    var volSpecial = $("#special").val();
+    if (volFirstName && volSecondName && volEmail && volNumber && volProf && volSpecial){
+      var newVolunter = new Volunter(volFirstName, volSecondName, volEmail, volNumber, volProf, volSpecial)
+      saveObjects('volunter-' + vdef, newVolunter);
+    }
+  })
 })
